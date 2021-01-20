9b0bd397587b8074dd411d7da384943e98c9a1b2
ryutaro.okita <ryutaro1408@gmail.com>
2021-01-20 13:31:29 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Ryutaro Okita on 2021/01/20.
+//  Copyright © 2021 ryutarou.okita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
